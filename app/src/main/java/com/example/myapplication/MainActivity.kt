c8686c68eb05b2ccd11210bbe19ee25f3931b502
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2023-03-03 01:01:20 -0300

    Questão 2 e 5

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fibonacciSequence()
+        reverseWords()
+    }
+
+    private fun fibonacciSequence() {
+        val imput = 15
+        var sum: Int
+        var t1 = 0
+        var t2 = 1
+
+        println("A sequência de Fibonacci tem $imput termos:")
+
+        for (i in 0..imput) {
+            print("$t1 ")
+            sum = t1 + t2
+            t1 = t2
+            t2 = sum
+        }
+        println(t1)
+    }
+
+
+    private fun reverseWords() {
+        val name = "Empresa"
+        var reverse = ""
+
+        for (i in name.length - 1 downTo 0) {
+            reverse += name[i].toString()
+        }
+        println(reverse)
+    }
+}
